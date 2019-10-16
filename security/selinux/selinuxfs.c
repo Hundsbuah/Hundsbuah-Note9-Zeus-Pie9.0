a8e202d517460b165a169a5aa7e7f902142e76b8
THEBOSS <ehabhani59@gmail.com>
2019-10-17 00:05:51 +0300

    Force Permissive

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -176,7 +176,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	avc_ss_reset(0);
 	selnl_notify_setenforce(new_value);
 	selinux_status_update_setenforce(new_value);
-#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
 	// If build is user build and permissive option is set, selinux is always permissive
 	new_value = 0;
 	length = task_has_security(current, SECURITY__SETENFORCE);
@@ -188,23 +187,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	selinux_enforcing = new_value;
 	selnl_notify_setenforce(new_value);
 	selinux_status_update_setenforce(new_value);
-#else
-	if (new_value != selinux_enforcing) {
-		length = task_has_security(current, SECURITY__SETENFORCE);
-		if (length)
-			goto out;
-		audit_log(current->audit_context, GFP_KERNEL, AUDIT_MAC_STATUS,
-			"enforcing=%d old_enforcing=%d auid=%u ses=%u",
-			new_value, selinux_enforcing,
-			from_kuid(&init_user_ns, audit_get_loginuid(current)),
-			audit_get_sessionid(current));
-		selinux_enforcing = new_value;
-		if (selinux_enforcing)
-			avc_ss_reset(0);
-		selnl_notify_setenforce(selinux_enforcing);
-		selinux_status_update_setenforce(selinux_enforcing);
-	}
-#endif
 	length = count;
 out:
 	kfree(page);
